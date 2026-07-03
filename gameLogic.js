65e25f4ec9dd814ebc806d8c51fb20142a31302e
HRT <294819316+HRTsandbox@users.noreply.github.com>
2026-07-03 10:00:50 +0200

    Set up the computer choice

--- gameLogic.js
+++ gameLogic.js
@@ -1,3 +1,10 @@
 console.log("Hello World")
 let random_n = Math.round((Math.random()*(3-1)+1))
+if (random_n === 1) {
+    console.log("Rock")
+} else if (random_n === 2) {
+    console.log("Paper")
+} else {
+    console.log("Scissors")
+}
 console.log(random_n)
