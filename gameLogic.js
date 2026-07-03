67e47fda1dac26b224ef1a228be4ddfd3f2484b6
HRT <294819316+HRTsandbox@users.noreply.github.com>
2026-07-03 10:51:27 +0200

    Draw up an aexperimental version of the game logic

--- gameLogic.js
+++ gameLogic.js
@@ -1,18 +1,42 @@
 function getComputerChoice() {
-    /*      Will have to return the choice as a parameter       */
     let random_n = Math.round((Math.random()*(3-1)+1))
+    let computerChoice = null
     if (random_n === 1) {
-        console.log("Rock")
+        computerChoice = "rock"
     } else if (random_n === 2) {
-        console.log("Paper")
+        computerChoice = "paper"
     } else {
-        console.log("Scissors")
+        computerChoice = "scissors"
     }
+    return computerChoice
 }
 function getHumanChoice() {
-    let choice = prompt("What is your next move?")
-    console.log(choice)
+    let choice = prompt("What is your next move?").toLowerCase()
+    return choice
 }
 
-getComputerChoice()
-getHumanChoice()
+let humanPoint = 0
+let computerPoint = 0
+
+for (let i=0; i<5; i+=1) {
+    let computerChoice = getComputerChoice()
+    let humanChoice = getHumanChoice()
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        computerPoint += 1
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanPoint += 1 
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanPoint += 1 
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        humanPoint += 1 
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        humanPoint += 1 
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanPoint += 1 
+    } else {
+        continue
+    }    
+}
+console.log(humanPoint)
+console.log(computerPoint)
+
